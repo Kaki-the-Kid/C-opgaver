50a5bc625229c20e09e4c70e7fff7a27aa99ea7c
Karsten Reitan Sørensen <karsten.reitan@gmail.com>
2021-10-30 20:28:04 +0200

    Code examples

---
+++ assigments/documentation/doc-example.c
@@ -0,0 +1,31 @@
+/**
+ @brief:
+ Relevant short description
+
+ @company:
+ Your company
+
+ File Name: main.c
+
+     or ...
+
+ @name:
+  
+ @summary:
+ This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+ @description:
+ What does it actualy do?
+
+ @version:  0.0.0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+}
+
+/**
+ End of File
+*/
